135bd3ee2ce5440c3b7ec645d9c14fc5ffbe44ed
Henrique Barbosa <hfb1@ifal.edu.br>
2024-06-12 21:56:44 -0300

    feat: add count words function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+
+    print(count_words(text))
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(text):
+    return len(text.split())
 
 main()
