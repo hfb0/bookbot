f2de0eab831a53da721298b97561e856fd859d7a
Henrique Barbosa <hfb1@ifal.edu.br>
2024-06-12 22:07:28 -0300

    feat: add count characteres function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
 
-    print(count_words(text))
+    print(count_characteres(text))
 
 
 def get_book_text(path):
@@ -11,5 +11,17 @@ def get_book_text(path):
 
 def count_words(text):
     return len(text.split())
+
+def count_characteres(text):
+    dict_count = {}
+    lowered_text = text.lower()
+
+    for char in lowered_text:
+        if(char in dict_count):
+            dict_count[char] += 1
+        else:
+            dict_count[char] = 1
+
+    return dict_count
 
 main()
