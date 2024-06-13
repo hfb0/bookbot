9fbdbaeebc4972f8d3ed9b745cf25c60eb7dc937
Henrique Barbosa <hfb1@ifal.edu.br>
2024-06-12 22:31:48 -0300

    feat: add report

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-
-    print(count_characteres(text))
-
+    print_report(book_path)
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,5 +20,26 @@ def count_characteres(text):
             dict_count[char] = 1
 
     return dict_count
+
+def print_report(book_path):
+    print(f"--- Begin report of {book_path} ---")
+
+    text = get_book_text(book_path)
+
+    count_w = count_words(text)
+    print(f"{count_w} words found in the document \n")
+
+    dict_c = count_characteres(text)
+    lst_c = list(dict_c.items())
+    lst_c.sort(reverse=True, key=sort_on_count)
+
+    for character, count_c in lst_c:
+        if(character.isalpha()):
+            print(f"The '{character}' character was found {count_c} times")
+
+    print("--- End report ---")
+
+def sort_on_count(tuple):
+    return tuple[1]
 
 main()
